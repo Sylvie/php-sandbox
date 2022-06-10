df15c693fe31eb8633f4c4931dce7347c70c8522
Sylvie <Sylvie@users.noreply.github.com>
2022-06-10 11:01:56 +0200

    #2: Affichage des données
     C'est moche, mais ça marche.

--- www/database.php
+++ www/database.php
@@ -9,11 +9,13 @@ $LINK = mysqli_connect("$hostname", "$username", "$password") or die("Could not 
 mysqli_set_charset($LINK, 'utf8');
 mysqli_select_db($LINK, "$nom_de_base") or die("Could not select database!");
 echo $LINK->host_info . "\n";
-
+echo  "\n<br />---<br/>";
 
 $result = mysqli_query($LINK, "Select * from user") or die('Error while fetching data.');
-var_dump(mysqli_fetch_array($result));
+while ($ligne = mysqli_fetch_array($result)) {
+    echo "$ligne[1] aime les $ligne[2]!<br />";
+ }
 echo  "\n---<br/>";
-echo " A la prochaine!";
+echo " A la prochaine! ";
 echo "Bye!";
 ?>

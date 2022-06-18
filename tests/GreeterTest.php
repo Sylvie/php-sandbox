532823d3b105c02d84235dde49aac263837e34c6
Sylvie Stucki <Sylvie@users.noreply.github.com>
2022-06-18 21:55:10 +0200

    #3: Ajout d'un tests avec deux personnes
     Le framework permet de tester les arguments lors d'appels consécutifs, et de renvoyer une réponse par appel.

--- tests/GreeterTest.php
+++ tests/GreeterTest.php
@@ -50,6 +50,35 @@ class GreeterTest extends \PHPUnit\Framework\TestCase {
 
         $this->assertEquals("Your favourite food is Cookies." , $result);
     }
+
+    public function testFindFavouriteFoodForTwoPeople() {
+        $resultMock = $this->createMock(mysqli_result::class);
+        $resultMock->expects($this->exactly(2))
+            ->method('fetch_array')
+            ->with()
+            ->willReturnOnConsecutiveCalls(
+                ["id" => 1, "name" => "Tina", "favourite_food" => "Cookies"],
+                ["id" => 2, "name" => "Phil", "favourite_food" => "Ice creams"]
+            );
+
+        $dbMock = $this->createMock(mysqli::class);
+        $dbMock->expects($this->exactly(2))
+            ->method('query')
+            ->withConsecutive(
+                [$this->equalTo("Select * from user where name='Tina'")],
+                [$this->equalTo("Select * from user where name='Phil'")]
+            )
+            ->willReturnOnConsecutiveCalls($resultMock, $resultMock);
+
+        $greeterTina = new \user_interactions\Greeter($dbMock, "Tina");
+        $greeterPhil = new \user_interactions\Greeter($dbMock, "Phil");
+
+        $resultTina = $greeterTina->findFavouriteFood();
+        $this->assertEquals("Your favourite food is Cookies." , $resultTina);
+
+        $resultPhil = $greeterPhil->findFavouriteFood();
+        $this->assertEquals("Your favourite food is Ice creams." , $resultPhil);
+    }
 }
 
 ?>
